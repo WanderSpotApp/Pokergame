9de8d2f8b182efb075d023876fed7de2594036c5
WanderSpotApp <eino@wanderspot.app>
2025-04-29 15:37:19 +0300

    oi

--- src/server.js
+++ src/server.js
@@ -41,7 +41,7 @@ app.use('/api/auth', authRoutes);
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../poker-frontend/build/index.html'));
 });
 
